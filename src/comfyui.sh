eeaa2dfac502dedfcfdd166e204ecb7cec8a2e20
Albers Uzila <alber.suzila@albers-uzila.0.1.168.192.in-addr.arpa>
2024-07-10 23:54:44 +0700

    Initial commit

--- src/comfyui.sh
+++ src/comfyui.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# define paths
+USER_NAME=$(whoami)
+USER_HOME=$(eval echo ~$USER_NAME)
+
 # install ComfyUI
 git clone https://github.com/comfyanonymous/ComfyUI
 pip install -r ComfyUI/requirements.txt
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
 pip install onnxruntime-gpu==1.18.0
+
+# install ComfyUI Manager
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git $USER_HOME/ComfyUI/custom_nodes/ComfyUI-Manager
+pip install -r $USER_HOME/ComfyUI/custom_nodes/ComfyUI-Manager/requirements.txt
 
 # test cuda
 echo -e "import torch\nprint(torch.cuda.is_available())\nprint(torch.cuda.get_device_name(0))" > test_cuda.py
