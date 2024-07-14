d4f1d346890260b0a955aa0087b9d43e192cbd88
Albers Uzila <dwiuzila@gmail.com>
2024-07-14 16:22:02 +0700

    Update extensions

--- src/extensions.sh
+++ src/extensions.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 
-# define paths
-USER_NAME=$(whoami)
-USER_HOME=$(eval echo ~$USER_NAME)
-
-# animatediff models
-path="$USER_HOME/ComfyUI/models/animatediff_models"
-wget https://huggingface.co/CiaraRowles/TemporalDiff/resolve/main/temporaldiff-v1-animatediff.safetensors -O $path/temporaldiff-v1-animatediff.safetensors
-
-# checkpoints
-path="$USER_HOME/ComfyUI/models/checkpoints"
-wget https://civitai.com/api/download/models/176342 -O $path/helloflatcolorful2d_V15e.safetensors
-
-# controlnet
-path="$USER_HOME/ComfyUI/models/controlnet"
-wget https://civitai.com/api/download/models/44811 -O $path/controlnet11Models_openpose.safetensors
-wget https://civitai.com/api/download/models/44879 -O $path/controlnet11Models_animeline.safetensors
-wget https://civitai.com/api/download/models/44736 -O $path/controlnet11Models_depth.safetensors
-wget https://civitai.com/api/download/models/67566 -O $path/controlnet11Models_tileE.safetensors
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11e_sd15_ip2p.pth -O $path/control_v11e_sd15_ip2p.pth
-
-# embeddings
-path="$USER_HOME/ComfyUI/models/embeddings"
-wget https://civitai.com/api/download/models/9208 -O $path/easynegative.safetensors
-wget https://civitai.com/api/download/models/25820 -O $path/verybadimagenegative_v1.3.pt
-wget https://civitai.com/api/download/models/125849 -O $path/bad-hands-5.pt
-
-# loras
-path="$USER_HOME/ComfyUI/models/loras"
-wget https://civitai.com/api/download/models/62833 -O $path/add_detail.safetensors
-
-# vae
-path="$USER_HOME/ComfyUI/models/vae"
-wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/vae/diffusion_pytorch_model.safetensors -O $path/diffusion_pytorch_model.safetensors
+wget --content-disposition https://civitai.com/api/download/models/438988?token=b400cf6ce58e881831d328b547a1788d -P /ComfyUI/models/checkpoints
+wget https://huggingface.co/guoyww/animatediff/resolve/main/v3_sd15_mm.ckpt -P /ComfyUI/models/animatediff_models
+wget https://huggingface.co/guoyww/animatediff/resolve/main/v3_sd15_adapter.ckpt -P /ComfyUI/models/loras
+wget https://huggingface.co/latent-consistency/lcm-lora-sdv1-5/resolve/main/pytorch_lora_weights.safetensors -P /ComfyUI/models/loras
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth -P /ComfyUI/models/controlnet
